be1ff1bd8c293694bd459a85b3f86294aeb52ccc
arkadiykadymov <arkadiymaestro@gmail.com>
2024-08-21 17:09:32 +0700

    Revert "new release"
    This reverts commit b42e325ffe4132d1a551c09b166e9296e79e53c1.

--- src/main/java/com/initflow/marking/base/models/ExportRequest.java
+++ src/main/java/com/initflow/marking/base/models/ExportRequest.java
@@ -1,10 +1,14 @@
 package com.initflow.marking.base.models;
 
+import java.util.List;
+
 public class ExportRequest<SR> {
     SR searchRequest;
     int userCount;
 
     SortingProperties sortingProperties;
+
+    List<String> emails;
 
     public SR getSearchRequest() {
         return searchRequest;
@@ -20,6 +24,14 @@ public class ExportRequest<SR> {
 
     public void setUserCount(int userCount) {
         this.userCount = userCount;
+    }
+
+    public List<String> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<String> emails) {
+        this.emails = emails;
     }
 
     public SortingProperties getSortingProperties() {
--- src/main/java/com/initflow/marking/base/util/loader/RestLoader.java
+++ src/main/java/com/initflow/marking/base/util/loader/RestLoader.java
@@ -1,35 +1,36 @@
 package com.initflow.marking.base.util.loader;
 
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class RestLoader {
 
     private Consumer<RestTemplate> restTemplateInfo;
 
-    public RestLoader() {
-    }
-
+    public RestLoader(){}
     public RestLoader(Consumer<RestTemplate> restTemplateInfo) {
         this.restTemplateInfo = restTemplateInfo;
     }
 
-    public <T> T get(String url, Map<String, String> headers, Class<T> zlass) {
+    public <T> T get(String url, Map<String, String> headers, Class<T> zlass){
         return getEntity(url, headers, zlass).getBody();
     }
 
-    public <T> ResponseEntity<T> getEntity(String url, Map<String, String> headers, Class<T> zlass) {
+    public <T> ResponseEntity<T> getEntity(String url, Map<String, String> headers, Class<T> zlass){
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setMessageConverters(getMessageConverters());
 
-        if (restTemplateInfo != null) {
+        if(restTemplateInfo != null) {
             restTemplateInfo.accept(restTemplate);
         }
 
@@ -37,27 +38,17 @@ public class RestLoader {
         httpHeaders.setAll(headers);
         HttpEntity entity = new HttpEntity(httpHeaders);
 
-        return restTemplate.exchange(url, HttpMethod.GET, entity, zlass);
+        return restTemplate.exchange(url, HttpMethod.GET,  entity, zlass);
     }
 
-    private List<HttpMessageConverter<?>> getMessageConverters() {
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
-        messageConverters.add(converter);
-
-        return messageConverters;
-    }
-
-    public <T, K> T post(String url, K params, Map<String, String> headers, Class<T> zlass) {
+    public <T,K> T post(String url, K params, Map<String, String> headers, Class<T> zlass){
         return postEntity(url, params, headers, zlass).getBody();
     }
 
-    public <T, K> ResponseEntity<T> postEntity(String url, K params, Map<String, String> headers, Class<T> zlass) {
+    public <T,K> ResponseEntity<T> postEntity(String url, K params, Map<String, String> headers, Class<T> zlass){
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setMessageConverters(getMessageConverters());
 
-        if (restTemplateInfo != null) {
+        if(restTemplateInfo != null) {
             restTemplateInfo.accept(restTemplate);
         }
 
@@ -68,11 +59,10 @@ public class RestLoader {
         return restTemplate.postForEntity(url, entity, zlass);
     }
 
-    public <K> void put(String url, K params, Map<String, String> headers) {
+    public <K> void put(String url, K params, Map<String, String> headers){
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setMessageConverters(getMessageConverters());
 
-        if (restTemplateInfo != null) {
+        if(restTemplateInfo != null) {
             restTemplateInfo.accept(restTemplate);
         }
 
@@ -84,11 +74,12 @@ public class RestLoader {
     }
 
 
-    public <T, K> T getObjectByRequest(String httpMethod, String url, K params, Map<String, String> headers,
-                                       Class<T> zlass) {
+
+    public <T,K> T getObjectByRequest(String httpMethod, String url, K params, Map<String, String> headers,
+                                             Class<T> zlass){
         headers = headers != null ? headers : new HashMap<>();
-        headers.put("Content-Type", "application/json;charset=UTF-8");
-        switch (httpMethod) {
+        headers.put("Content-Type","application/json;charset=UTF-8");
+        switch (httpMethod){
             case "GET":
                 return get(url, headers, zlass);
             case "POST":
@@ -101,11 +92,11 @@ public class RestLoader {
     }
 
 
-    public <T, K> ResponseEntity<T> getResponseEntityByRequest(String httpMethod, String url, K params, Map<String, String> headers,
-                                                               Class<T> zlass) {
+    public <T,K> ResponseEntity<T> getResponseEntityByRequest(String httpMethod, String url, K params, Map<String, String> headers,
+                                      Class<T> zlass){
         headers = headers != null ? headers : new HashMap<>();
-        headers.put("Content-Type", "application/json;charset=UTF-8");
-        switch (httpMethod) {
+        headers.put("Content-Type","application/json;charset=UTF-8");
+        switch (httpMethod){
             case "GET":
                 return getEntity(url, headers, zlass);
             case "POST":
